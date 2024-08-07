8cdcb6e668efe2d5bc8dcaf0f812eff2fcdb2c40
aryan <aryanlakhani2001@gmail.com>
2024-08-07 19:04:31 -0400

    Update index.js adding comments

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+// get references to the elements
 const myButton = document.getElementById("myButton");
 const myLabel = document.getElementById("myLabel");
 const diceImage = document.getElementById("diceImage");
+
+// set min and max values for random number
 const min = 1;
 const max = 100; 
-let randomNum;
 
+// function to generate and display a random number
 function rollDice() {
-    randomNum = Math.floor(Math.random() * max) + min;
+    const randomNum = Math.floor(Math.random() * max) + min;
     myLabel.textContent = randomNum;
 }
 
+// attach event listeners to the button and image
 myButton.onclick = rollDice;
 diceImage.onclick = rollDice;
